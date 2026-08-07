55592301cbc03dac5e80b0d2dbc14ed7297bbec9
PradhumnaAdhikari <pradhumnaadhikari2@gmail.com>
2026-08-07 16:49:29 +0000

    feat:Created a setup scripting file for creating directories

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,31 +2,15 @@
 
 project_root="$(cd "$(dirname "$0")/.." && pwd )"
 
-app="$project_root/app"
-docker="$project_root/docker"
-terraform="$project_root/terraform"
-logging="$project_root/logging"
-scripts="$project_root/scripts"
+directories=("app" "docker" "terraform" "logging" "scripts")
 
-if [ ! -d "$app" ]; then
-	echo "Creating /app directory"
-	mkdir -p "$app"
+for dir in "${directories[@]}"; do
+	dir_root=$project_root/$dir
 
-elif [ ! -d "$docker" ]; then
-	echo "Creating /docker directory"
-	mkdir -p "$docker"
-
-elif [ ! -d "$terraform" ]; then
-	echo "Creating /terraform directory"
-	mkdir -p "$terraform"
-	
-elif [ ! -d "logging" ]; then
-	echo "Creating /logging directory"
-	mkdir -p "$logging"
-
-elif [ ! -d "$scripts"];then
-	echo "Creating /scripts directory"
-	mkdir -p "$scripts"
-else
-	echo "Directory already exists"
-fi
+	if [ ! -d "$dir_root" ]; then
+		echo "creating the /$dir directory"
+		mkdir -p "$dir_root"
+	else 
+		echo "/$dir directory already exists"
+	fi
+done
